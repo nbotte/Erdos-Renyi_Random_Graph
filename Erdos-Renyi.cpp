775fd381fa44f7d31c8523ed953b98df3eb2eed8
Nina Botte <nina.botte@ugent.be>
2020-09-18 10:30:03 +0200

    test

---
+++ Erdos-Renyi.cpp
@@ -0,0 +1,10 @@
+#define _USE_MATH_DEFINES
+#include <cmath>
+#include <math.h>
+#include <string>
+#include <fstream>
+#include <iomanip>
+#include <iostream>
+#include <vector>
+#include <time.h>
+using namespace std;
