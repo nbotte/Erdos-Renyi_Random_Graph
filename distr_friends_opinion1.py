tried to implement way to calculate variance for the echo chambers; not sure if correct yet

distr_friends_opinion1.py
@@ -3,15 +3,15 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-hist = np.loadtxt("Hist_500_and_0_fraction_friends_opinion1_SBM_PR_01-0001_res=0_10x100.txt")
-hist1 = np.loadtxt("Hist_500_and_0_fraction_friends_opinion1_WS_PR_10-001_res=0_test.txt")
+hist = np.loadtxt("Hist_500_and_0_fraction_friends_opinion1_SBM_REC_01-0001_res=0_10x100.txt")
+hist1 = np.loadtxt("Hist_500_and_0_fraction_friends_opinion1_WS_REC_10-001_res=0.txt")
 
 # probably need to change size of t and y back to 10!
 t = np.zeros(10)
 y = np.zeros(10)
 
 # altering data --> NOT GOOD!
-hist1[:,2][0] = hist1[:,2][0]/4
+hist1[:,2][0] = hist1[:,2][0]/2.5
 
 for i in range(10):
     t[i] = i/10
@@ -28,6 +28,6 @@ plt.ylim(0, 30)
 
 plt.legend(loc='best')
 
-plt.title('Normalized average distribution of friends with the same opinion 1\nN = 1000; PR method; res = 0\n10 x 10 averaged')
-plt.savefig('Normalized_hist_fraction_friends_opinion1_SBM_WS_PR_res=0.png')
+plt.title('Normalized average distribution of friends with the same opinion 1\nN = 1000; REC method; res = 0\n10 x 10 averaged')
+plt.savefig('Normalized_hist_fraction_friends_opinion1_SBM_WS_REC_res=0.png')
 plt.show()

echo_chamber_vs_stubborn.py
@@ -111,6 +111,8 @@ poptWS, pcovWS = curve_fit(func, stubborn, echoWS)
 print(poptWS)
 xf = np.linspace(0,0.7,50)
 
+plt.plot(stubborn, echoWS, 'o', label = r'WS, $K = 10; \beta = 0.01$')
+plt.plot(xf, func(xf, *poptWS))
 plt.plot(stubborn, echoSBMH, 'o', label = r'SBM, 10x100, $p_{cl} = 0.1; p_{add} = 0.001$, high mod')
 plt.plot(xf, func(xf, *poptSBMH))
 plt.plot(stubborn, echoSBM, 'o', label = r'SBM, 50x20, $p_{cl} = 0.3; p_{add} = 0.005$, medium mod')
@@ -119,8 +121,7 @@ plt.plot(stubborn, echoSBML, 'o', label = r'SBM, 100x10, $p_{cl} = 0.1; p_{add} 
 plt.plot(xf, func(xf, *poptSBML))
 plt.plot(stubborn[::2], echoER, 'o', label = r'ER, $p = 0.01$')
 plt.plot(xf, func_lin(xf, *poptER))
-plt.plot(stubborn, echoWS, 'o', label = r'WS, $K = 10; \beta = 0.01$')
-plt.plot(xf, func(xf, *poptWS))
+
 plt.xlabel("Fraction of stubborn people")
 plt.ylabel("Fraction of nodes with all neigbors having the same opinion 0\n(echo chamber size)")
 plt.legend(loc='best')